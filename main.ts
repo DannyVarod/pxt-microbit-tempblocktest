7da009cd965275c66ea98e922a3421cf6064f710
DannyVarod <danny.varod+github@gmail.com>
2021-01-16 03:34:15 +0200

    Initial commit

---
+++ main.ts
@@ -0,0 +1,96 @@
+/**
+ * Testing blocks layout in menu.
+ */
+//% color="#303030" weight=100 icon="\uf0e7"
+//% groups='["GroupA", "GroupB", "GroupC", "GroupD", "GroupE"]'
+namespace tempTest {
+
+    let myClass: MyClass = null
+
+    /**
+     * Button state
+     */
+    export enum ButtonState {
+        //% block=Released
+        Released = 0,
+        //% block=Pressed
+        Pressed = 1
+    }
+
+    function hiddenFunction(buttonState: ButtonState): Number {
+        return buttonState.valueOf()
+    }
+
+    /**
+     * Initialize something
+     * @param buttonState button state
+     */
+    //% block="Initialize"
+    //% blockId="tempTest_intialize|button State %buttonState"
+    //% port.fieldEditor="gridpicker" port.fieldOptions.columns=2
+    //% group="GroupA"
+    //% weight=90
+    export function intialize(buttonState: ButtonState): void {
+        hiddenFunction(buttonState)
+        myClass = new MyClass(0)
+    }
+
+    /**
+     * Return something
+     * @param buttonState button state
+     */
+    //% block="ReturnSomething"
+    //% blockId="returnSomething_intialize|button State %buttonState"
+    //% port.fieldEditor="gridpicker" port.fieldOptions.columns=2
+    //% group="GroupB"
+    //% weight=87
+    export function returnSomething(buttonState: ButtonState): string {
+        return buttonState.toString()
+    }
+
+    /**
+     * Get something
+     */
+    //% block="GetSomething"
+    //% blockId="getSomething_intialize"
+    //% port.fieldEditor="gridpicker" port.fieldOptions.columns=2
+    //% group="GroupC"
+    //% weight=89
+    export function getSomething(): ButtonState {
+        return ButtonState.Pressed
+    }
+
+    /**
+     * Abstraction for led matrices
+     */
+    export class MyClass {
+        x: number
+
+        constructor(x: number) {
+            this.x = x
+        }
+    
+        /**
+         * Set x
+         * @param x x [0,255]
+        */
+        public setX(x: number): void {
+            this.x = x
+        }
+    }
+
+    /**
+     * Set x
+     * @param x x [0,255]
+    */
+    //% block="Set x|x %x"
+    //% blockId="microX_setX"
+    //% x.min=0 x.max=255
+    //% x.fieldEditor="gridpicker" x.fieldOptions.columns=2
+    //% inlineInputMode=inline
+    //% group="GroupD"
+    //% weight=30
+    export function setX(x: number): void {
+        myClass.setX(x)
+    }
+}
